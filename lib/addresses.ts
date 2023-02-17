91316bd5861578a5c6dc6b3589eedc022b1dd270
Callum McIntyre <mcintyre1994@gmail.com>
2023-02-17 11:53:31 +0000

    Support connecting wallet and sending transaction

---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from '@solana/web3.js'
+
+// Your shop wallet address
+export const shopAddress = new PublicKey(
+  'EkMGcCfkrs4Eqrd9C4CEhPDRY2Es8SPQs6cGWiBYVKzt'
+)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,35 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import Head from 'next/head'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { clusterApiUrl } from '@solana/web3.js'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = clusterApiUrl(network);
+
   return (
-    <Layout>
-      <Head>
-        <title>Cookies Inc</title>
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <Layout>
+            <Head>
+              <title>Cookies Inc</title>
+            </Head>
+            <Component {...pageProps} />
+          </Layout>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
