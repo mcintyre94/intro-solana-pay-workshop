7f17f5517b1b4856e9eface836b2accdec2154d7
Callum McIntyre <mcintyre1994@gmail.com>
2023-02-17 12:01:57 +0000

    Detect payment + add confirmed page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,6 +1,7 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { Keypair, Transaction } from "@solana/web3.js";
+import { findReference, FindReferenceError } from "@solana/pay";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
 import BackLink from "../components/BackLink";
@@ -88,6 +89,27 @@ export default function Checkout() {
   useEffect(() => {
     trySendTransaction()
   }, [transaction])
+
+  // Check every 0.5s if the transaction is completed
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        // Check if there is any transaction for the reference
+        const signatureInfo = await findReference(connection, reference);
+        router.push('/confirmed')
+        clearInterval(interval)
+      } catch (e) {
+        if (e instanceof FindReferenceError) {
+          // No transaction found yet, ignore this error
+          return;
+        }
+        console.error('Unknown error', e)
+      }
+    }, 500)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
   if (!publicKey) {
     return (
