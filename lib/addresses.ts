ba0ccf52b8c3e4fa4f3cf02195c3a8ed3a09e0bd
Callum McIntyre <mcintyre1994@gmail.com>
2023-02-17 12:22:37 +0000

    Charge in USDC instead of SOL

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -4,3 +4,8 @@ import { PublicKey } from '@solana/web3.js'
 export const shopAddress = new PublicKey(
   'EkMGcCfkrs4Eqrd9C4CEhPDRY2Es8SPQs6cGWiBYVKzt'
 )
+
+// This is the fake USD address from splfaucet.lol
+export const usdcAddress = new PublicKey(
+  '4HZCNvobxtDA3uezTGmDAEqVLp7oo73UrnbxNeUMszd4'
+)
